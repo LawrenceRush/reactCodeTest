9cd2d1ae656b592249ec105941896c424fbfc4d2
Lawrence <l.rush7@gmail.com>
2020-01-31 21:15:53 -0800

    broke namepage into 2 components

---
+++ client/src/components/NamePageInfo.js
@@ -0,0 +1,124 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { QuizContext } from '../store/store'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { useSpring, animated, useTransition } from 'react-spring'
+
+function NamePageInfo() {
+  let [nameAbsent, isNameAbsent] = useState(false)
+  const [toggle, on] = useState(false)
+  let { score } = useContext(QuizContext)
+  let { beAnonymous } = useContext(QuizContext)
+  let { name } = useContext(QuizContext)
+  let { handleInputChange } = useContext(QuizContext)
+  let { time } = useContext(QuizContext)
+  let { addScore } = useContext(QuizContext)
+  let { setPage } = useContext(QuizContext)
+  useEffect(() => {
+    if (name != "") {
+      isNameAbsent(false)
+    }
+  })
+const namePageInfoCon = {
+  height: "100vh",
+  display: "grid",
+  gridTemplateColumns: "auto",
+  alignItems: "center"
+}
+  const infoStyles = {
+    fontSize: '4vh',
+    textAlign: 'center'
+
+  }
+  const buttonStyle = {
+    textAlign: "Center"
+  }
+
+  const textStyle = {
+    textAlign: "center",
+    fontSize: "3vh"
+  }
+  const numberCircleStyle = {
+    display: 'inline-block',
+    width: "1.5em",
+    height: "1.5em",
+    textAlign: 'center',
+    borderRadius: "50%",
+    margin: '1rem auto',
+    background: "DeepSkyBlue"
+  }
+
+  let scoreAnim = useSpring({
+    delay: score * 250,
+    to: { number: score },
+    from: { number: 0 },
+    config: { duration: (score * 500) }
+  })
+
+  let timeAnim = useSpring({
+    delay: 250,
+    to: { number: time },
+    from: { number: 0 },
+    config: { duration: (time * 25) }
+  })
+
+  const delayChange = () => {
+    if (name !== "") {
+      on(!toggle)
+      const interval = setInterval(function () {
+        setPage("HighScore")
+        clearInterval(interval)
+      }, 250);//run this thang every 2 seconds
+    } else {
+
+      isNameAbsent(true)
+    }
+
+  }
+
+  return (
+    <div style = {namePageInfoCon}>
+      <div>
+      <div style={infoStyles}> Score:
+      <animated.div style={numberCircleStyle}>
+          {scoreAnim.number.interpolate(val => (score !== 0) ? Math.floor(val) : 0)}
+        </animated.div>
+      </div>
+
+      <div style={infoStyles} >Time left:
+          <animated.div style={numberCircleStyle}>
+          {timeAnim.number.interpolate(val => (time !== 0) ? Math.floor(val) : 0)}
+        </animated.div>
+      </div>
+
+
+      <div style={{ textAlign: "Center" }}>
+        <TextField style={{ fontSize: "4vh" }} id="standard-basic" value={name}
+          onChange={(e) => { handleInputChange(e) }}
+          label="Enter name here"
+          error={nameAbsent}
+          helperText={nameAbsent ? 'Empty field!' : ' '}
+        />
+      </div>
+      <div style={textStyle}>
+        OR
+    </div>
+      <div style={buttonStyle}>
+        <Button style={{ fontSize: "3vh" }} color="secondary" onClick={(e) => { beAnonymous() }}>
+          Be anyonymous
+      </Button>
+      </div>
+      <div style={textStyle}>
+        Then
+    </div>
+      <div style={buttonStyle}>
+        <Button style={{ fontSize: "3vh" }} color="primary" onClick={(e) => { addScore(); delayChange() }}>
+          View high scores!
+      </Button>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default NamePageInfo
--- client/src/pages/NamePage.js
+++ client/src/pages/NamePage.js
@@ -1,100 +1,50 @@
 import React,{useContext, useEffect, useState} from 'react'
 import {QuizContext} from '../store/store'
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import {useSpring, animated, useTransition} from 'react-spring'
 import {
    round
 } from 'mathjs'
+import NamePageInfo from "../components/NamePageInfo"
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: 200,
-    },
-  },
-}));
-// 
+
+
 function NamePage() {
-  let[nameAbsent, isNameAbsent] = useState(false)
-  const[toggle, on] = useState(true)
-    const classes = useStyles();
-    let {score} = useContext(QuizContext)
-    let {beAnonymous} = useContext(QuizContext)
-    let {name} = useContext(QuizContext)
-    let {handleInputChange} = useContext(QuizContext)
-    let {time} = useContext(QuizContext)
-    let {addScore} = useContext(QuizContext)
-
+  const[toggle, on] = useState(false)
+   
     let {setPage} = useContext(QuizContext)
+
     useEffect(()=>{
-       console.log( Math.floor(0))
-      })
+      on(false)
+      },[])
+
 const namePageCon= {
-  display: "flex",
-  justifyContent: "center",
-  flexDirection: "column",
-  height: "100vh",
-  
-  
-  
-}
-
-const infoStyles = {
-   fontSize: '4vh',
- textAlign:'center'
-
-}
-const buttonStyle = {
-  textAlign: "Center"
-}
-
-const textStyle = {
-  textAlign:"center",
-  fontSize: "3vh"
-}
-const numberCircleStyle = {
-  display:'inline-block',
-  width: "1.5em",
-  height:"1.5em",
-  textAlign:'center',
-  borderRadius: "50%",
-  margin: '1rem auto',
-background: "DeepSkyBlue"
-}
-
-useEffect(()=> {
-if (name != ""){
-  isNameAbsent(false)
-}
-})
+  position: "relative"
+  }
 
 const slides = useTransition(toggle, null, {
     
   from: {
-    width: '30vh',
-      textAlign:'left',
+    
+      
       position: 'absolute',
       transform: "translate3d(250vh,0,0)" ,
    
   },
   enter: { 
-    width: '30vh',
+    
 
-      textAlign:'left',
+      
       position: 'absolute',
-     
-      transform: "translate3d(0vh,0,0)",
-      left: 0,
-      right: 0,
-      margin: "auto"
+     ransform: "translate3d(0vh,0,0)",
+      
+      
       
       },
   leave: {
-    width: '30vh',
-      textAlign:'left',
+    
+      
       position: 'absolute',
       transform: "translate3d(-250vh,0,0)",
   },
@@ -102,65 +52,19 @@ const slides = useTransition(toggle, null, {
 });
 
 
-//spring for raising number
-let scoreAnim = useSpring({ 
-    delay:  score * 250,
-    to:{number: score}, 
-    from: { number: 0 }, 
-    config: {duration:(score * 500)} 
-  })
-
-  let timeAnim = useSpring({ 
-    delay:  250,
-    to:{number: time}, 
-    from: { number: 0 }, 
-    config: {duration:(time * 25)} 
-  })
 
 
-const delayChange = () => {
-  if(name !== ""){
-    on(!toggle)
-  const interval = setInterval(function(){ 
-    setPage("HighScore")
-    clearInterval(interval)
-}, 250);//run this thang every 2 seconds
-  } else{
 
-    isNameAbsent(true)
-  }
-  
-}
+
 
     return (
-<div style = {namePageCon}>
+<div id = "namePageContainer" style = {namePageCon}>
 {slides.map(({ item, key, props }) => (
   item &&
-<animated.form className={classes.root} style = {props} noValidate autoComplete="off">
-<div style = {infoStyles}> Score: 
-<animated.div style={numberCircleStyle}>{scoreAnim.number.interpolate(val => (score !== 0) ? Math.floor(val): 0)}
-</animated.div> </div>
-
-    <div style = {infoStyles} >Time left:  
-    
-    <animated.div style={numberCircleStyle}>{timeAnim.number.interpolate(val => (time !== 0) ? Math.floor(val): 0)}
-</animated.div> </div>
-    
-    
-    <TextField style = {{fontSize: "4vh"}} id="standard-basic" value = {name} 
-    onChange = {(e) => {handleInputChange(e)}} 
-    label="Enter name here" 
-    error={nameAbsent}
-    helperText={nameAbsent ? 'Empty field!' : ' '}
-    />
-    <div style= {textStyle}>OR</div>
-    <div style = {buttonStyle}> <Button style = {{fontSize:"3vh"}} color="secondary" onClick={(e) => {beAnonymous()} }>Be anyonymous</Button></div>
-    <div style= {textStyle}>Then</div>
-    <div style = {buttonStyle}>   <Button style = {{fontSize:"3vh"}} color="primary" onClick={(e) => {addScore(); delayChange()} }>View high scores!</Button>
-    
-</div>
+<animated.form   noValidate autoComplete="off">
+<NamePageInfo/>
 </animated.form>
-      ))}
+      ))} 
       </div>
     )
 }
